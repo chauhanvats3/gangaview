62f5ff2a6651d21824596ebdb1e627283073a91e
chauhanvats3 <Co11iourse>
2021-01-22 14:03:17 +0530

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<style>
+	.hero-section {
+		width: 100vw;
+		height: 100vh;
+		flex-flow: column wrap;
+		justify-items: flex-end;
+		position: relative;
+		overflow: hidden;
+		background-image: url("../images/sunset.jpg");
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+	}
+
+	.banner {
+		width: 100%;
+	}
+
+	.info {
+		width: 100%;
+		margin: 2vh;
+		color: white;
+		justify-self: flex-end;
+		text-shadow: 0 0 5px black, 0 0 3px black;
+
+	}
+</style>
+
+<div class="hero-section">
+	<div class="banner">
+		<div>
+			Rooms Starting @ Rs. 699 <br />Open 24 X 7
+		</div>
+		<div></div>
+	</div>
+	<div class="info">Scroll Down To See More</div>
+</div>
