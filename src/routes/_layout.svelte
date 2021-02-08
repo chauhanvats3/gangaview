ab464ee370b9d897ce3bc2497e316d226857dd28
chauhanvats3 <chauhan.vats3.com>
2021-02-08 22:54:49 +0530

    added loading bar

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,14 @@
 <script>
 	import Nav from '../components/Nav.svelte';
+	import { fade } from "svelte/transition"
+	import { stores } from "@sapper/app"
+	import PageLoadingBar from "sapper-page-loading-bar/PageLoadingBar.svelte"
 	import { onMount } from 'svelte';
+
 	export let segment;
+
+	const { preloading } = stores()
+
 
 	onMount(() => {
 		window.onscroll = function (e) {
@@ -12,6 +19,8 @@
 	})
 </script>
 
+
+
 <style>
 	main {
 		width: 100%;
@@ -21,6 +30,10 @@
 
 <Nav {segment} />
 
-<main>
+<PageLoadingBar {preloading} height="3px" color1="white" color2="#00171f" />
+
+{#if !$preloading}
+<main transition:fade>
 	<slot></slot>
 </main>
+{/if}
