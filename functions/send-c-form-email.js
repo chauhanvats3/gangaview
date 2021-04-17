7382ae908509a7c2f8903e122d57209be46a3420
Vatsal Chauhan <chauhan.vats3@gmail.com>
2021-04-18 00:18:22 +0530

    getting ready for mail

--- functions/send-c-form-email.js
+++ functions/send-c-form-email.js
@@ -1,11 +1,36 @@
+const fs = require('fs').promises;
+
+let createJSONFile = async (data, name) => {
+    await fs.writeFile(`files/${name}`, JSON.stringify(data), function (err) {
+        if (err) {
+            return { saved: false, "err": err }
+        }
+    });
+    return { saved: true };
+}
+
 exports.handler = async function (event, context) {
     const params = event.queryStringParameters;
     const dataset = JSON.parse(params.dataset);
-    console.log("send : " + dataset.basic.f_name)
+
+    const fileName = dataset.passport.number + ".json"
+
+    let fileStatus = await createJSONFile(dataset, fileName);
+
+    if (!fileStatus.saved) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "File Was Not Saved!" })
+        };
+    }
+
+    console.log("After Save")
+
     zoho_pass = process.env.ZOHO_AUTH_PASS;
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello " + dataset.basic.f_name })
+        body: JSON.stringify({ message: "File saved and Mail Sent!" })
     };
+
 }
